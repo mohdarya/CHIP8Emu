5623879c34ad938378471de06cbe8a9d1129e261
mohdarya <mohsen.a.daryanavard@gmail.com>
2022-01-05 18:49:29 +0400

    feat: outputing content of program file

--- Chip8.cpp
+++ Chip8.cpp
@@ -2,4 +2,27 @@
 // Created by moh on 1/5/22.
 //
 
+
+
 #include "Chip8.h"
+#include <fstream>
+using namespace std;
+
+
+void Chip8::loadProgram(string fileName) {
+    ifstream myFile (fileName, ios::binary);
+    if(myFile.is_open())
+    {
+        streampos fileSize = myFile.tellg();
+        char* buffer = new char[fileSize];
+
+        myFile.read(buffer, fileSize);
+        myFile.close();
+
+        for(long i = 0; i < fileSize; i++)
+        {
+            cout << buffer[i]
+        }
+    }
+
+}
