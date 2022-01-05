63d0d4e74fc58109be99037a33f2df45d81678b8
mohdarya <mohsen.a.daryanavard@gmail.com>
2022-01-05 19:15:18 +0400

    loading program into memory

--- Chip8.cpp
+++ Chip8.cpp
@@ -6,23 +6,31 @@
 
 #include "Chip8.h"
 #include <fstream>
+#include <iostream>
 using namespace std;
 
-
+const unsigned int START_ADDRESS = 0x200;
 void Chip8::loadProgram(string fileName) {
-    ifstream myFile (fileName, ios::binary);
+
+    ifstream myFile (fileName, ios::binary| ios::ate);
+
     if(myFile.is_open())
     {
+
         streampos fileSize = myFile.tellg();
         char* buffer = new char[fileSize];
 
+        myFile.seekg(0, ios::beg);
         myFile.read(buffer, fileSize);
         myFile.close();
 
+
         for(long i = 0; i < fileSize; i++)
         {
-            cout << buffer[i]
+            memory[START_ADDRESS + i] = buffer[i];
         }
+
+        delete[] buffer;
     }
 
 }
