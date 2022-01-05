d95ff9c2ccd850ffd3cadb817f4bb66028d51686
mohdarya <mohsen.a.daryanavard@gmail.com>
2022-01-05 17:57:29 +0400

    feat: declared chip variables

---
+++ Chip8.cpp
@@ -0,0 +1,5 @@
+//
+// Created by moh on 1/5/22.
+//
+
+#include "Chip8.h"
---
+++ Chip8.h
@@ -0,0 +1,28 @@
+//
+// Created by moh on 1/5/22.
+//
+#include <cstdint>
+#ifndef CHIP8EMU_CHIP8_H
+#define CHIP8EMU_CHIP8_H
+
+
+class Chip8 {
+
+private:
+    uint8_t registers[16]{};
+    uint8_t memory [4096]{};
+    uint16_t indexReg;
+    uint16_t programCounter;
+    uint16_t stack[16]{};
+    uint8_t stackPointer;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint8_t keys[16]{};
+    uint32_t pixels[64][32]{};
+
+public:
+    
+};
+
+
+#endif //CHIP8EMU_CHIP8_H
